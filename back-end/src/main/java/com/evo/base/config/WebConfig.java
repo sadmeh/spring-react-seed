62072b5cca2b5d40db46153a24f190f3f1c861d5
mehdi <sadmeh@gmail.com>
2019-10-15 12:35:20 +0330

    fix: production jar bundle forwarded to index.html

--- back-end/src/main/java/com/evo/base/config/WebConfig.java
+++ back-end/src/main/java/com/evo/base/config/WebConfig.java
@@ -3,10 +3,10 @@ package com.evo.base.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 //    @Profile("dev")
     @Override
