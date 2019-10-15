f350dca9ea48b5075f1da3cde849fb674608bb1c
mehdi <sadmeh@gmail.com>
2019-10-15 13:49:32 +0330

    test: rest controller

--- back-end/src/main/java/com/evo/base/controllers/IndexController.java
+++ back-end/src/main/java/com/evo/base/controllers/IndexController.java
@@ -1,13 +1,20 @@
 package com.evo.base.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.ws.rs.HttpMethod;
+
+@RestController
+@RequestMapping("/api")
 public class IndexController {
 
-//    @GetMapping("/")
-//    public String index(){
-//        return "index";
-//    }
+    @GetMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String index(){
+        return "index14444";
+    }
 
 }
