3fb740bbef0109f3a0eca71fbb765a7d5e280d32
mehdi <sadmeh@gmail.com>
2019-10-15 16:35:01 +0330

    remove: unused dependencies from backend module.

--- bundle/src/main/java/com/evo/bundle/Application.java
+++ bundle/src/main/java/com/evo/bundle/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.evo.backend.*"})
-public class Application  {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
